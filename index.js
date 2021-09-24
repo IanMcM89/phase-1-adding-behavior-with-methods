d7a6603eb2d70498d497ef4d89d29382269da349
Ian <ianmcm.89@gmail.com>
2021-09-24 12:47:13 -0400

    commit tree 0a24e3e08e150e80b040a4b3b923faefe454e87e

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Your code here
+class Cat{
+    constructor(name, sex){
+        this.name = name;
+        this.sex = sex;
+    }
+    speak(){
+       return `${this.name} says meow!`;
+    }
+}
+
+class Dog{
+    constructor(name, sex){
+        this.name = name;
+        this.sex = sex;
+    }
+    speak(){
+        return `${this.name} says woof!`;
+    }
+}
+
+class Bird{
+    constructor(name, sex){
+        this.name = name;
+        this.sex = sex;
+    }
+    speak(sex){
+        if(this.sex === 'male'){
+            return `It\'s me! ${this.name}, the parrot!`;
+        } else {
+            return `${this.name} says squawk!`;
+        }
+    }
+}
+
+let cat = new Cat('Apollo', 'male');
+
+let dog = new Dog('Kali', 'female');
+
+let birdM = new Bird('Oscar', 'male');
+
+let birdF = new Bird('Mindy', 'female');
+
+console.log(cat.speak());
+console.log(dog.speak());
+console.log(birdM.speak());
+console.log(birdF.speak());
